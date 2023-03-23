634ff77b2520a69481b7226e6147ebc36f16e307
Vinicius Duarte <viniaduarte@outlook.com>
2023-03-23 18:01:05 -0300

    feat: create SubmitButton component

---
+++ apps/web/ui/form/SubmitButton.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@chakra-ui/react";
+
+interface Props {
+  text: string;
+  isDisabled: boolean;
+  onClick?: () => void;
+  isLoading?: boolean;
+}
+
+export default function SubmitButton({
+  text,
+  onClick,
+  isDisabled,
+  isLoading,
+}: Props) {
+  return (
+    <Button
+      type="submit"
+      onClick={onClick}
+      isLoading={isLoading}
+      isDisabled={isDisabled}
+      bgColor="brand.primary"
+      color={"brand.white"}
+      width="100%"
+      fontSize={"18px"}
+      fontWeight={"800"}
+      height={"70px"}
+      _hover={{
+        bgColor: "brand.primary",
+      }}
+    >
+      {text}
+    </Button>
+  );
+}
