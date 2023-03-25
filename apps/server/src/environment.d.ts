8532089eba25a2d4c3d40eef51fd3f76828f468c
Vinicius Duarte <viniaduarte@outlook.com>
2023-03-24 21:17:41 -0300

    refactor: set environment types in root dir

---
+++ apps/server/src/environment.d.ts
@@ -0,0 +1,11 @@
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production";
+      PORT?: string;
+      MONGO_URI: string;
+    }
+  }
+}
