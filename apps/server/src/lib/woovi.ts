1df8a7a857477b1b46ffd2600233496534f522eb
Vinicius Duarte <viniaduarte@outlook.com>
2023-03-25 02:22:10 -0300

    feat: create WooviCreateChargeReponse at woovi lib

--- apps/server/src/lib/woovi.ts
+++ apps/server/src/lib/woovi.ts
@@ -8,6 +8,35 @@ export interface WooviCreateChargeDto {
   expiresIn: 9999999999999;
 }
 
+export interface WooviCreateChargeReponse {
+  charge: {
+    customer: null;
+    value: number;
+    comment: string;
+    identifier: string;
+    correlationID: string;
+    paymentLinkID: string;
+    transactionID: string;
+    status: string;
+    giftbackAppliedValue: number;
+    discount: number;
+    valueWithDiscount: number;
+    expiresDate: null;
+    type: string;
+    createdAt: string;
+    additionalInfo: [];
+    updatedAt: string;
+    brCode: string;
+    expiresIn: number;
+    pixKey: string;
+    paymentLinkUrl: string;
+    qrCodeImage: string;
+    globalID: string;
+  };
+  correlationID: string;
+  brCode: string;
+}
+
 export class Woovi {
   private client: AxiosInstance;
 
@@ -17,13 +46,15 @@ export class Woovi {
     this.client = axios.create({ baseURL });
 
     this.client.interceptors.request.use((config) => {
-      config.headers["Authorization"] = `Bearer ${process.env.WOOVI_API_KEY}`;
+      config.headers["Authorization"] = process.env.WOOVI_API_KEY;
       return config;
     });
   }
 
   charge = {
-    create: async (data: WooviCreateChargeDto) => {
+    create: async (
+      data: WooviCreateChargeDto
+    ): Promise<WooviCreateChargeReponse> => {
       const response = await this.client.post("/api/v1/charge", data);
       return response.data;
     },
