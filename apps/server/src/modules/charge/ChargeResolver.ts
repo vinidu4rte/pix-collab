48ec2ed2f4e455a6bfb895947cd0830aa09336af
Vinicius Duarte <viniaduarte@outlook.com>
2023-03-25 02:49:19 -0300

    feat: complete ChargeResolver saving in database, making requests and using transaction

--- apps/server/src/modules/charge/ChargeResolver.ts
+++ apps/server/src/modules/charge/ChargeResolver.ts
@@ -6,11 +6,14 @@ import {
   FieldResolver,
   Root,
 } from "type-graphql";
+import { db } from "../../config/database";
+import { Woovi, WooviCreateChargeDto } from "../../lib/woovi";
 import { Charge } from "./dtos/entities/Charge";
 import { PartialCharge } from "./dtos/entities/PartialCharge";
 import { CreateChargeInput } from "./dtos/inputs/CreateChargeInput";
 import ChargeModel from "./models/ChargeModel";
 import { PartialChargeModel } from "./models/PartialChargeModel";
+import { randomUUID } from "crypto";
 
 @Resolver(() => Charge)
 export class ChargeResolver {
@@ -21,22 +24,54 @@ export class ChargeResolver {
 
   @Mutation(() => Charge)
   async createCharge(@Arg("data") data: CreateChargeInput) {
-    const { collaboratorsQuantity, value } = data;
+    const session = await db.getInstance().startSession();
 
-    const charge = await ChargeModel.create({
-      value,
-      collaboratorsQuantity,
-    });
+    try {
+      session.startTransaction();
+      const { collaboratorsQuantity, value } = data;
 
-    return charge;
+      const charge = await ChargeModel.create({
+        value,
+        collaboratorsQuantity,
+      });
+
+      const partialChargeValue = value / collaboratorsQuantity;
+
+      const woovi = new Woovi();
+
+      for (let i = 0; i < collaboratorsQuantity; i++) {
+        const correlationId = randomUUID();
+
+        const chargeDto: WooviCreateChargeDto = {
+          correlationID: correlationId,
+          comment: `Cobrança ${i + 1}`,
+          expiresIn: 9999999999999,
+          value: partialChargeValue,
+        };
+
+        const response = await woovi.charge.create(chargeDto);
+        const { qrCodeImage, transactionID } = response.charge;
+
+        await PartialChargeModel.create({
+          chargeId: charge.id,
+          value: partialChargeValue,
+          correlationId,
+          qrCode: qrCodeImage,
+          transactionId: transactionID,
+        });
+      }
+
+      session.commitTransaction();
+      return charge;
+    } catch (error) {
+      session.abortTransaction();
+      throw error;
+    }
   }
 
   @FieldResolver(() => [PartialCharge]!)
-  async partialCharge(@Root() charge: Charge) {
-    const { id } = charge;
-
-    const partialCharges = PartialChargeModel.find({ chargeId: id });
-
-    return partialCharges;
+  async partialCharge(@Root() charge: Charge | any) {
+    const chargeId = charge._doc._id;
+    return await PartialChargeModel.find({ chargeId });
   }
 }
