3efe3be0eb9fdac4b7fcb0a27159aa25c5a1081d
Vinicius Duarte <viniaduarte@outlook.com>
2023-03-27 19:28:30 -0300

    feat: query graphql api to get charge info based in pathname

---
+++ apps/web/pages/charge/[id].tsx
@@ -0,0 +1,89 @@
+import { gql, useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
+import Layout from "../../ui/generic/Layout";
+import ChargeCompleted from "../../ui/specific/ChargeCompleted";
+import QrCodeCharge from "../../ui/specific/QrCodeCharge";
+
+const GET_CHARGE = gql`
+  query Charge($chargeId: String!) {
+    charge(id: $chargeId) {
+      id
+      status
+      value
+      collaboratorsQuantity
+      partialCharge {
+        id
+        transactionId
+        qrCode
+        status
+        value
+      }
+    }
+  }
+`;
+
+interface PartialCharge {
+  id: string;
+  status: "pending" | "paid";
+  qrCode: string;
+  value: number;
+}
+
+interface ChargeData {
+  id: string;
+  status: "pending" | "paid";
+  collaboratorsQuantity: number;
+  value: number;
+  partialCharge: PartialCharge[];
+}
+
+export default function Charge() {
+  const router = useRouter();
+  const chargeId = router.query.id;
+
+  const { data, loading, error } = useQuery<{ charge: ChargeData }>(
+    GET_CHARGE,
+    {
+      variables: {
+        chargeId: chargeId,
+      },
+    }
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error || !data) {
+    return <div>Error</div>;
+  }
+
+  const { charge } = data;
+  const { id, status, value, partialCharge } = charge;
+
+  if (status === "paid") {
+    return (
+      <ChargeCompleted
+        paymentId={id}
+        receiverName="VD Consultoria"
+        totalValue={value / 100}
+      />
+    );
+  }
+
+  return (
+    <Layout>
+      {partialCharge.map((charge: PartialCharge, index: number) => (
+        <QrCodeCharge
+          key={charge.id}
+          id={charge.id}
+          status={charge.status}
+          value={charge.value / 100}
+          paymentNumber={index}
+          qrCode={charge.qrCode}
+          hasDivider={index !== partialCharge.length - 1}
+        />
+      ))}
+    </Layout>
+  );
+}
