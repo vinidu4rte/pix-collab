75b6a8c6884cf810eb7955b2609498166bfc9c6c
Vinicius Duarte <viniaduarte@outlook.com>
2023-03-28 20:21:29 -0300

    refactor: remove not related code in formatCurrency

--- apps/web/utils/formatCurrency.ts
+++ apps/web/utils/formatCurrency.ts
@@ -1,13 +1,5 @@
-export function formatCurrency(value: string) {
-  const numericValue = Number(value.replace(/[^0-9]/g, "")) / 100;
-
-  if (isNaN(numericValue)) return value.replace(/[^0-9]/g, "");
-
-  if (numericValue > 5000) {
-    return "R$ 5.000,00";
-  }
-
-  return numericValue.toLocaleString("pt-BR", {
+export function formatCurrency(value: number) {
+  return value.toLocaleString("pt-BR", {
     style: "currency",
     currency: "BRL",
   });
