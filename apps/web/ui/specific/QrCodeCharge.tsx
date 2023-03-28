138f6dc392b4f5b674abe8856d18ae375b1ee5e4
Vinicius Duarte <viniaduarte@outlook.com>
2023-03-28 11:46:17 -0300

    feat: integration simulate pay button with server

--- apps/web/ui/specific/QrCodeCharge.tsx
+++ apps/web/ui/specific/QrCodeCharge.tsx
@@ -1,9 +1,16 @@
+import { gql, useMutation } from "@apollo/client";
 import { Box, Center, VStack, Text, Divider } from "@chakra-ui/react";
 import Image from "next/image";
 import SubmitButton from "../generic/form/SubmitButton";
 import Success from "../generic/svg/Success";
 import PageTitle from "../generic/text/PageTitle";
 import TextWithLabel from "../generic/text/TextWithLabel";
+
+const FAKE_CHARGE_PAYMENT = gql`
+  mutation Mutation($data: FakeChargePaymentInput!) {
+    fakeChargePayment(data: $data)
+  }
+`;
 
 interface Props {
   id: string;
@@ -22,6 +29,18 @@ export default function QrCodeCharge({
   qrCode,
   hasDivider,
 }: Props) {
+  const [createCharge, { loading }] = useMutation(FAKE_CHARGE_PAYMENT);
+
+  const onPayButtonClick = async () => {
+    await createCharge({
+      variables: {
+        data: {
+          transactionId: id,
+        },
+      },
+    });
+  };
+
   const formattedCurrency = value.toLocaleString("pt-BR", {
     currency: "BRL",
     style: "currency",
@@ -77,8 +96,10 @@ export default function QrCodeCharge({
               fontSize="12px"
               height={8}
               width={150}
-              isDisabled={false}
+              isDisabled={loading}
+              isLoading={loading}
               borderRadius={0}
+              onClick={onPayButtonClick}
             />
           </>
         )}
