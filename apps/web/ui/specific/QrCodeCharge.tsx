05fa937798fead3ace8d7796d97720502ae0b0fd
Vinicius Duarte <viniaduarte@outlook.com>
2023-04-02 15:41:35 -0300

    refactor: set image quality as 40

--- apps/web/ui/specific/QrCodeCharge.tsx
+++ apps/web/ui/specific/QrCodeCharge.tsx
@@ -88,6 +88,7 @@ export default function QrCodeCharge({
                 sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               33vw"
+                quality={40}
                 priority={true}
               />
             </Box>
