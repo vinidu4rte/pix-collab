560ce1d9e76eae1f3fcacd6f0a84d6f07067dd78
Vinicius Duarte <viniaduarte@outlook.com>
2023-04-11 19:57:08 -0300

    refactor: define relay provider as nextjs + relay starter suggests

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,19 +2,21 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { customizedTheme } from "../theme";
 import Head from "next/head";
-import { RelayEnvironmentProvider } from "react-relay";
-import { environment } from "../relay/RelayEnvironment";
+import { ReactRelayContext } from "react-relay";
+import { useEnvironment } from "../relay/RelayEnvironment";
 
 function App({ Component, pageProps }: AppProps) {
+  const environment = useEnvironment(pageProps.initialRecords);
+
   return (
-    <RelayEnvironmentProvider environment={environment}>
+    <ReactRelayContext.Provider value={{ environment }}>
       <ChakraProvider theme={customizedTheme}>
         <Head>
           <title>PixCollab - Woovi</title>
         </Head>
         <Component {...pageProps} />
       </ChakraProvider>
-    </RelayEnvironmentProvider>
+    </ReactRelayContext.Provider>
   );
 }
 
